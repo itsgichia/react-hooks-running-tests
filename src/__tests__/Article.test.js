86063b865f6a93d770c977ea46202e8c4275182c
itsgichia <gichiamuiruri@gmail.com>
2024-01-29 20:18:44 +0300

    assignment completed

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,12 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  waitFor(()=>{
+    expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  });
 });
--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,11 +1,12 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 
 import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
-
-  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  waitFor(() => {
+    expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+  });
 });
